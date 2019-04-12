4d2d92ef96cdee0cda47663576dd3276c21f11e8
badrubadri92@gmail.com <baddrubadri92@gmail.com>
2019-04-12 15:42:47 +0300

    deployment to heroku

---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY='d5b2ab9e53f62ccbbb7d3d3fbc75b247'
+export SECRET_KEY='badar'
+
+python3.7 manage.py server
---
+++ tests/test_movie.py
@@ -0,0 +1,26 @@
+import unittest
+from app.models import Movie
+
+class MovieTest(unittest.TestCase):
+    '''
+    Test Class to test the behavior of the movie class
+    '''
+
+    def setUp(self):
+        '''
+        set up method that will run before every Test
+        '''
+
+        self.new_movie = Movie(1234,'python must be crazy','A thrilling new python series','/khsjha27hbs',8.5,129993)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_movie,Movie))
+
+
+    # def test_init(self):
+    #     self.assertEqual(self.new_movie.id,1234)
+    #     self.assertEqual(self.new_movie.title,'python must be crazy')
+    #     self.assertEqual(self.new_movie.overview,'A thrilling new python series')
+    #     self.assertEqual(self.new_movie.poster,'https://image.tmdb.org/t/p/w500/khsjha27hbs')
+    #     self.assertEqual(self.new_movie.vote_average,8.5)
+    #     self.assertEqual(self.new_movie.vote_count,129993)
